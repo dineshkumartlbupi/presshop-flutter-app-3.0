e55a39abfd3404c7cfb2286008b0d8c792da1b7f
Difmo <difmo@Difmos-Mac-mini.local>
2026-02-24 00:00:43 +0530

    url changed

--- lib/core/api/api_constant.dart
+++ lib/core/api/api_constant.dart
@@ -18,17 +18,17 @@ class Config {
   static const int env = 1;
   // "http://localhost:8100/api/";
 
-  // =============>>S
-  // String get baseUrl =>
-  //     "https://lelia-anthracitic-ecclesiologically.ngrok-free.dev/api/";
-
-  // String get socketUrl2 =>
-  //     "wss://lelia-anthracitic-ecclesiologically.ngrok-free.dev";
-
+  // =============>>
   String get baseUrl =>
-      "https://funnellike-subangular-sulema.ngrok-free.dev/api/";
+      "https://lelia-anthracitic-ecclesiologically.ngrok-free.dev/api/";
 
-  String get socketUrl2 => "wss://funnellike-subangular-sulema.ngrok-free.dev";
+  String get socketUrl2 =>
+      "wss://lelia-anthracitic-ecclesiologically.ngrok-free.dev";
+
+  // String get baseUrl =>
+  //     "https://funnellike-subangular-sulema.ngrok-free.dev/api/";
+
+  // String get socketUrl2 => "wss://funnellike-subangular-sulema.ngrok-free.dev";
 
   String get googleMapURL =>
       "https://maps.googleapis.com/maps/api/place/autocomplete/json";
