682caf0347818c3222d170e0348de13e07a4d48a
difmo <difmotech@gmail.com>
2026-04-20 10:19:45 +0530

    save

--- lib/core/api/api_constant.dart
+++ lib/core/api/api_constant.dart
@@ -18,16 +18,16 @@ class Config {
   static const int env = 1;
 
   // ====================== Ngrok Url =====================
-  // String get baseUrl =>
-  //     "https://lelia-anthracitic-ecclesiologically.ngrok-free.dev/api/";
+  String get baseUrl =>
+      "https://lelia-anthracitic-ecclesiologically.ngrok-free.dev/api/";
 
-  // String get socketUrl2 =>
-  //     "wss://lelia-anthracitic-ecclesiologically.ngrok-free.dev";
+  String get socketUrl2 =>
+      "wss://lelia-anthracitic-ecclesiologically.ngrok-free.dev";
 
   // ====================== Ngrok Url =====================
-  String get baseUrl =>
-      "https://funnellike-subangular-sulema.ngrok-free.dev/api/";
-  String get socketUrl2 => "wss://funnellike-subangular-sulema.ngrok-free.dev";
+  // String get baseUrl =>
+  //     "https://funnellike-subangular-sulema.ngrok-free.dev/api/";
+  // String get socketUrl2 => "wss://funnellike-subangular-sulema.ngrok-free.dev";
 
 // ====================== Localhost Url =====================
 
