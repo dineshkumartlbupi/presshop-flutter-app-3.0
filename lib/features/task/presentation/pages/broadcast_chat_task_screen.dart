940d8044f0cc0425d3029d5cdf469d5ca052973d
difmo <difmotech@gmail.com>
2026-05-05 16:20:20 +0530

    save

--- lib/features/task/presentation/pages/broadcast_chat_task_screen.dart
+++ lib/features/task/presentation/pages/broadcast_chat_task_screen.dart
@@ -296,24 +296,7 @@ class _BroadCastChatTaskScreenState extends State<BroadCastChatTaskScreen> {
           appBar: CommonBrandedAppBar(
             title: AppStringsNew2.manageTaskText,
             size: size,
-<<<<<<< HEAD
             showLogo: true,
-=======
-            showActions: true,
-            leadingFxn: () {
-              context.pop();
-            },
-            actionWidget: [
-              InkWell(
-                  onTap: () {
-                    context.goNamed(
-                      AppRoutes.dashboardName,
-                      extra: {'initialPosition': 2},
-                    );
-                  },
-                  child: LogoWidget.buildLogo(size)),
-            ],
->>>>>>> 024697f4060b8e2d999232bee70406f9f3e7a7ed
           ),
           bottomNavigationBar: (isDataLoading && chatList.isNotEmpty)
               ? showLoader()
